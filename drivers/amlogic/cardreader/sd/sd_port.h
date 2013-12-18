2dcf8e4addda965f8fda373b718f4c9746e4a510
Stanislav Vlasic <svlasic@gmail.com>
2013-12-18 20:41:07 +0100

    Add SD_MMC_NO_WP_CHECK option to cardreader driver

--- drivers/amlogic/cardreader/sd/sd_port.h
+++ drivers/amlogic/cardreader/sd/sd_port.h
@@ -59,7 +59,9 @@ extern unsigned SD_PWR_EN_LEVEL;
 
 extern unsigned SD_WORK_MODE;
 #define SD_MMC_POWER_CONTROL
+#ifndef CONFIG_SD_MMC_NO_WP_CHECK
 #define SD_MMC_WP_CHECK
+#endif
 
 extern void sd_sdio_enable(SDIO_Pad_Type_t io_pad_type);
 extern void sd_gpio_enable(SDIO_Pad_Type_t io_pad_type);
